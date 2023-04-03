961dc38ed71361d7d8ffbdbe5050dd0f02faa08d
ReflexTen <bulanov.roman4@gmail.com>
2023-04-03 21:53:11 +0500

    added functionality

--- src/App.js
+++ src/App.js
@@ -10,61 +10,25 @@ import About from './components/sections/About'
 import Portfolio from './components/sections/Portfolio'
 import Contacts from './components/sections/Contacts'
 
-// window.onscroll = () => {
-// console.log(window)
-// }
-
 function App() {
-  const menuIcon = React.createRef()
-  const navBar = React.createRef()
-  const about = useRef()
-  const home = useRef()
+  const menuIcon = useRef()
+  const navBar = useRef()
+
   const portfolio = useRef()
   const contacts = useRef()
+  const homeRef = useRef()
+  const aboutRef = useRef()
 
   function loh() {
     menuIcon.current.classList.toggle('active-menu')
     navBar.current.classList.toggle('active')
   }
 
-  const [activeLink, setActiveLink] = useState('Home')
+  const [activeLink, setActiveLink] = useState('Главный экран')
 
   const activeLinkHandler = link => {
     setActiveLink(link)
   }
-
-  setTimeout(() => {
-    // console.log(home.current)
-    // console.log(about)
-
-    window.onscroll = () => {
-      const top = window.scrollY
-      const offsetAbout = about.current.offsetTop - 150
-      const heightAbout = about.current.offsetHeight
-
-      const offsetHome = home.current.offsetTop - 150
-      const heightHome = home.current.offsetHeight
-
-      const offsetPortfolio = portfolio.current.offsetTop - 150
-      const heightPortfolio = portfolio.current.offsetHeight
-
-      const offsetContacts = contacts.current.offsetTop - 200
-      const heightContacts = contacts.current.offsetHeight
-
-      if (top >= offsetHome && top < offsetHome + heightHome) {
-        activeLinkHandler('Home')
-      }
-      if (top >= offsetAbout && top < offsetAbout + heightAbout) {
-        activeLinkHandler('About')
-      }
-      if (top >= offsetPortfolio && top < offsetPortfolio + heightPortfolio) {
-        activeLinkHandler('Portfolio')
-      }
-      if (top >= offsetContacts && top < offsetContacts + heightContacts) {
-        activeLinkHandler('Contacts')
-      }
-    }
-  }, 1)
 
   return (
     <div className="App">
@@ -72,15 +36,13 @@ function App() {
         menuIcon={menuIcon}
         navBar={navBar}
         loh={loh}
-        homeRef={home}
-        aboutRef={about}
         activeLink={activeLink}
         activeLinkClick={activeLinkHandler}
       />
 
-      <Main>
-        <Home homeRef={home} />
-        <About aboutRef={about} />
+      <Main activeLinkScroll={activeLinkHandler}>
+        <Home homeRef={homeRef} />
+        <About aboutRef={aboutRef} />
         <Portfolio portfolio={portfolio} />
         <Contacts contacts={contacts} />
       </Main>
--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,6 +1,6 @@
-const NavLink = ({ text, href, activeLink, activeLinkClick }) => {
+const NavLink = ({ text, href, activeLink, activeLinkClick, loh }) => {
   return (
-    <li className="navbar__item">
+    <li className="navbar__item" onClick={loh}>
       <a
         className={`navbar__link ${activeLink == text ? 'active' : ''}`}
         href={href}
