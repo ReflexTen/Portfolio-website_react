11a0de2378105d870b61bcbcd0919c003b83cf8a
ReflexTen <bulanov.roman4@gmail.com>
2023-04-05 20:47:03 +0500

    added animation

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,76 +1,86 @@
 import React from 'react'
-import { useRef } from 'react'
 import myPhoto from './../../images/photos/sticker.png'
 
+import Fade from 'react-reveal/Fade'
+
 const Home = ({ homeRef }) => {
+  const properties = {
+    duration: 3000,
+    distance: '80px',
+    delay: 200,
+  }
+
   return (
     <section ref={homeRef} className="home section" id="home">
-      <div className="home__content">
-        <h1 className="home__title title">
-          <span>Front-end React </span>разработчик
-        </h1>
+      <Fade top {...properties}>
+        <div className="home__content">
+          <h1 className="home__title title">
+            <span>Front-end React </span>разработчик
+          </h1>
 
-        <p className="home__text">
-          Привет, меня зовут Буланов Роман. Увлеченный Front-end React
-          разработчик из России.
-        </p>
+          <p className="home__text">
+            Привет, меня зовут Буланов Роман. Увлеченный Front-end React
+            разработчик из России.
+          </p>
 
-        <ul className="home__social-media social-media">
-          <li className="social-media__item">
-            <a className="social-media__link" href="#">
-              <i className="bx bxl-whatsapp"></i>
-            </a>
-          </li>
-          <li className="social-media__item">
-            <a className="social-media__link" href="#">
-              <i className="bx bxl-telegram"></i>
-            </a>
-          </li>
-          <li className="social-media__item">
-            <a className="social-media__link" href="#">
-              <i className="bx bxl-instagram"></i>
-            </a>
-          </li>
-          <li className="social-media__item">
-            <a className="social-media__link" href="#">
-              <i className="bx bxl-vk"></i>
-            </a>
-          </li>
-        </ul>
-
-        <div className="home__tech-stack">
-          <div className="home__stacks-text">Технологические стеки</div>
-          <ul className="home__stacks">
-            <li className="home__stack">
-              <i className="bx bxl-html5"></i>
+          <ul className="home__social-media social-media">
+            <li className="social-media__item">
+              <a className="social-media__link" href="#">
+                <i className="bx bxl-whatsapp"></i>
+              </a>
             </li>
-
-            <li className="home__stack">
-              <i className="bx bxl-css3"></i>
+            <li className="social-media__item">
+              <a className="social-media__link" href="#">
+                <i className="bx bxl-telegram"></i>
+              </a>
             </li>
-
-            <li className="home__stack">
-              <i className="bx bxl-sass"></i>
+            <li className="social-media__item">
+              <a className="social-media__link" href="#">
+                <i className="bx bxl-instagram"></i>
+              </a>
             </li>
-
-            <li className="home__stack">
-              <i className="bx bxl-javascript"></i>
-            </li>
-
-            <li className="home__stack">
-              <i className="bx bxl-react"></i>
-            </li>
-
-            <li className="home__stack">
-              <i className="bx bxl-github"></i>
+            <li className="social-media__item">
+              <a className="social-media__link" href="#">
+                <i className="bx bxl-vk"></i>
+              </a>
             </li>
           </ul>
-        </div>
-      </div>
 
-      <div className="home__img-box">
-        <img className="home__img" src={myPhoto} alt="фото автора" />
-      </div>
+          <div className="home__tech-stack">
+            <div className="home__stacks-text">Технологические стеки</div>
+            <ul className="home__stacks">
+              <li className="home__stack">
+                <i className="bx bxl-html5"></i>
+              </li>
+
+              <li className="home__stack">
+                <i className="bx bxl-css3"></i>
+              </li>
+
+              <li className="home__stack">
+                <i className="bx bxl-sass"></i>
+              </li>
+
+              <li className="home__stack">
+                <i className="bx bxl-javascript"></i>
+              </li>
+
+              <li className="home__stack">
+                <i className="bx bxl-react"></i>
+              </li>
+
+              <li className="home__stack">
+                <i className="bx bxl-github"></i>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Fade>
+      <Fade bottom {...properties}>
+        <div className="home__img-box">
+          <img className="home__img" src={myPhoto} alt="фото автора" />
+        </div>
+      </Fade>
     </section>
   )
 }
--- src/components/sections/Portfolio.jsx
+++ src/components/sections/Portfolio.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
-import { useRef } from 'react'
-
+import Fade from 'react-reveal/Fade'
 import Project from '../Project'
 import projectList from './../helpers/projectList'
 
 const Portfolio = ({ portfolio }) => {
+  const properties = {
+    duration: 3000,
+    distance: '80px',
+    delay: 200,
+  }
+
   return (
     <section ref={portfolio} className={`portfolio section`} id="portfolio">
-      <h2 className="portfolio__heading heading">
-        Latest <span>project</span>
-      </h2>
-
-      <ul className="portfolio__container">
-        {projectList.map(project => (
-          <Project
-            key={project.id}
-            src={project.src}
-            title={project.title}
-            text={project.text}
-            link={project.link}
-          />
-        ))}
-      </ul>
+      <Fade top {...properties}>
+        <h2 className="portfolio__heading heading">
+          Latest <span>project</span>
+        </h2>
+      </Fade>
+      <Fade bottom {...properties}>
+        <ul className="portfolio__container">
+          {projectList.map(project => (
+            <Project
+              key={project.id}
+              src={project.src}
+              title={project.title}
+              text={project.text}
+              link={project.link}
+            />
+          ))}
+        </ul>
+      </Fade>
     </section>
   )
 }
